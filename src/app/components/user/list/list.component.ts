6a4846f4037e0de1c12c57146127fe2ee1fe8d87
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2022-12-22 13:08:15 -0400

    feat: 🎨component List Users

---
+++ src/app/components/user/list/list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Users } from 'src/app/shared/models/users.model';
+import { UsersService } from 'src/app/users/services/users.service';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent  implements OnInit  {
+
+  users: Users[] = []
+  displayedColumns = ['id', 'name', 'email', 'phone', 'email','status', 'action']
+
+  constructor(private userService: UsersService) { }
+
+  ngOnInit(): void {
+    this.userService.read().subscribe(users => {
+      this.users = users
+    })
+  }
+
+}
