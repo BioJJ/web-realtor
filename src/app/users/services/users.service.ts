a71da40601c192ce4ac1f35ad12bfc55e732d102
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2022-12-22 09:47:41 -0400

    feat: Users service

---
+++ src/app/users/services/users.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import * as config from '../../config/config';
+import { HttpClient } from '@angular/common/http';
+import { Users } from 'src/app/shared/models/users.model';
+import { Observable, EMPTY } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersService {
+  baseUrl: string = `${config}/users`;
+  constructor(private snackBar: MatSnackBar, private http: HttpClient) {}
+
+  showMessage(msg: string, isError: boolean = false): void {
+    this.snackBar.open(msg, 'X', {
+      duration: 3000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+      panelClass: isError ? ['msg-error'] : ['msg-success'],
+    });
+  }
+
+  create(user: Users): Observable<Users> {
+    return this.http.post<Users>(this.baseUrl, user).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
+  }
+
+  read(): Observable<Users[]> {
+    return this.http.get<Users[]>(this.baseUrl).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
+  }
+
+  readById(id: number): Observable<Users> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.get<Users>(url).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
+  }
+
+  update(user: Users): Observable<Users> {
+    const url = `${this.baseUrl}/${user.id}`;
+    return this.http.put<Users>(url, user).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
+  }
+
+  delete(id: number): Observable<Users> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.delete<Users>(url).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
+  }
+
+  errorHandler(e: any): Observable<any> {
+    this.showMessage('Ocorreu um erro!', true);
+    return EMPTY;
+  }
+}
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { UsersService } from './services/users.service';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  providers: [UsersService],
 })
-export class UsersModule { }
+export class UsersModule {}
