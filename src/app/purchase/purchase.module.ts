b7ccbdd067029ca565697523984fa97f50794f70
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2022-12-22 09:47:59 -0400

    feat: Purchase service

--- src/app/purchase/purchase.module.ts
+++ src/app/purchase/purchase.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { PurchaseService } from './services/purchase.service';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  providers: [PurchaseService],
 })
-export class PurchaseModule { }
+export class PurchaseModule {}
