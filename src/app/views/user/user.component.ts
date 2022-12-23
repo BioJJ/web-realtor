cfefa3d823b9ed4447cf7703f9f3107ce44e7295
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2022-12-23 05:32:32 -0400

    feat: model login, userToken

--- src/app/views/user/user.component.ts
+++ src/app/views/user/user.component.ts
@@ -11,7 +11,7 @@ export class UserComponent implements OnInit {
   constructor(private router: Router, private headerService: HeaderService) {
     headerService.headerData = {
       title: 'Cadastro de usuario',
-      icon: 'storefront',
+      icon: 'person',
       routeUrl: '/users',
     };
   }
